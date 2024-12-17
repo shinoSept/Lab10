7385861426ee2696c7cb714b8eac913876a14dbd
Chinoros <chinoros2815@gmail.com>
2024-12-17 15:47:44 +0700

    edited

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,29 @@
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <string>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int count = 0;
+    double sum = 0;
+    double sumSquare = 0;
+    string score;
+    ifstream source("score.txt");
+    while (getline(source,score)) {
+        sum += atof(score.c_str());
+        sumSquare += pow(atof(score.c_str()),2);
+        count++;
+    }
+    cout << "Number of data = " << count << endl;
+    cout << setprecision(3);
+    cout << "Mean = " << sum / count << endl;
+    cout << "Standard deviation = " << sqrt( (sumSquare/count) - ( (sum*sum)/(count*count) ) );
+}
+
+
 //cout << "Number of data = ";
 //cout << "Mean = ";
 //cout << "Standard deviation = ";
